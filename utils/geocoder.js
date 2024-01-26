c78b5cd2d66854cadd82e3eba4fe372843647703
Jide Abdul-Qudus <jideabdqudus@gmail.com>
2024-01-26 00:50:18 +0000

    feat: add gelocation api

---
+++ utils/geocoder.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+const NodeGeoCoder = require("node-geocoder");
+dotenv.config({ path: "./config/config.env" });
+
+const options = {
+  provider: `${process.env.GEOCODER_PROVIDER}`,
+  httpAdapter: "https",
+  apiKey: `${process.env.GEOCODER_API_KEY}`,
+  formatter: null,
+};
+
+const geocoder = NodeGeoCoder(options);
+
+module.exports = geocoder;
